2a2e867b4291b5c9fcee93cf64f6b0c53d6819f0
Shilpa Eppa <seppa@mtm-inc.net>
2025-05-08 11:17:11 +0530

    Add project files.

---
+++ DockerApp/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok("Welcome to Products service");
+        }
+
+    }
+}
