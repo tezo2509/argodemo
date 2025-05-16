c838c9dd942c5e355cf6d20ca87fd61c28f1dec9
Paramesh Kenchugundu <seppa@mtm-inc.net>
2025-05-16 16:14:43 +0530

    Added Reverse Proxy Nginx and logger

--- DockerApp/Controllers/ProductController.cs
+++ DockerApp/Controllers/ProductController.cs
@@ -8,18 +8,21 @@ namespace DockerApp.Controllers
     public class ProductController : Controller
     {    
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<ProductController> _logger;
 
-        public ProductController(ApplicationDbContext context)
+        public ProductController(ApplicationDbContext context,ILogger<ProductController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
-    {
-        var products = await _context.Products.ToListAsync();
-        return Ok(products);
-    }
-
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var appName=Environment.GetEnvironmentVariable("App_Name");
+            _logger.LogInformation($"Serving request using {appName}");
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
     }
 }
